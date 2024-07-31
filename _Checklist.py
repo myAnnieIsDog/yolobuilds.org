8ab8a3662437f2050217160c1fc2088585e30c1a
myAnnieIsDog <146695251+myAnnieIsDog@users.noreply.github.com>
2024-07-30 19:46:22 -0700

    Actualizar _Checklist.py

--- _Checklist.py
+++ _Checklist.py
@@ -1,87 +1,134 @@
-_bl = {
-    "Demolition": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "Electrical": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "Existing": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "Fire": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "Flood": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "Grading": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "Mechanical": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "Manufactured_home": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "New Accessory": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "New Commercial": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "New Residential": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "Plumbing": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
-    "Pool": {
-        "models": "",
-        "views": "",
-        "templates": "",
-    },
+_profiles = {
+    "models": "65%",
+    "views": "30%",
+    "templates": "10%",
+    "testing": "0%",
+}
+_land = {
+    "models": "65%",
+    "views": "",
+    "templates": "",
+    "testing": "0%",
+}
+_reviews = {
+    "models": "65%",
+    "views": "",
+    "templates": "",
+    "testing": "0%",
+}
+_inspections = {
+    "models": "65%",
+    "views": "",
+    "templates": "",
+    "testing": "0%",
 }
 _bp = {
-    "models": "",
+    "models": "65%",
     "views": "",
     "templates": "",
+    "testing": "0%",
+}
+_bl = {
+    "models": "0%",
+    "views": "",
+    "templates": "",
+    "testing": "0%",
+}
+_bp = {
+    "Demolition": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "Electrical": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "Existing": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "Fire": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "Flood": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "Grading": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "Mechanical": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "Manufactured_home": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "New Accessory": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "New Commercial": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "New Residential": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "Plumbing": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
+    "Pool": {
+        "models": "65%",
+        "views": "",
+        "templates": "",
+        "testing": "0%",
+    },
 }
 _ce = {
-    "models": "",
+    "models": "0%",
     "views": "",
     "templates": "",
+    "testing": "0%",
 }
 _pw = {
-    "models": "",
+    "models": "0%",
     "views": "",
     "templates": "",
+    "testing": "0%",
 }
 _zf = {
-    "models": "",
-    "views": "",
-    "templates": "",
+    "models": "0%",
+    "views": "0%",
+    "templates": "0%",
+    "testing": "0%",
 }
